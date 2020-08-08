0f6e7dadcb032be1b7825fddbe9a2cf168df1ed7
Jan Bader <jan.bader@ub-software.de>
2020-08-09 00:16:59 +0200

    Add answer endpoint

--- cmd/web.go
+++ cmd/web.go
@@ -32,6 +32,7 @@ var webCmd = &cobra.Command{
 
 		server.Server.Get("/api/stats", server.getStats)
 		server.Server.Get("/api/learn", server.learn)
+		server.Server.Post("/api/answer", server.answer)
 		server.Server.Get("/(.*)", server.static)
 		http.ListenAndServe(":9876", server.Server)
 	},
@@ -47,6 +48,26 @@ func (s *vocabularyServer) learn(ctx *web.Context){
 	words := s.Vocabulary.GetSortedWords([]string{})
 	jsonEncoder := json.NewEncoder(ctx.ResponseWriter)
 	jsonEncoder.Encode(words)
+}
+
+type answerRequest struct {
+	Word string
+	Success bool
+}
+
+func (s *vocabularyServer) answer(ctx *web.Context){
+	req := answerRequest{}
+	jsonDecoder := json.NewDecoder(ctx.Request.Body)
+	jsonDecoder.Decode(&req)
+
+	stats := s.Vocabulary.GetStats(req.Word)
+	if req.Success {
+		stats.CorrectAnswer()
+	} else {
+		stats.FalseAnswer()
+	}
+
+	s.Vocabulary.Save(*wordsfilename, *statsfilename)
 }
 
 func (s *vocabularyServer) static(ctx *web.Context, filepath string){
